7aca8c5f532853bbf8904d70174065403e85f59a
Admin <hesanqq@gmail.com>
2022-09-08 17:51:54 +0430

    Repository3

--- app/src/main/java/com/example/rxjavamvvmnew/view/fragments/FragmentPost.java
+++ app/src/main/java/com/example/rxjavamvvmnew/view/fragments/FragmentPost.java
@@ -31,45 +31,42 @@ public class FragmentPost extends Fragment {
     NavController navController;
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        if (binding==null){
-        binding= FragmentPostBinding.inflate(inflater,container,false);
-        ViewModelPost viewModelPost=new ViewModelProvider(this).get(ViewModelPost.class);
-        viewModelPost.posts();
-        viewModelPost.mutableLiveData.observe(requireActivity(), new Observer<List<Model_Posts>>() {
-            @Override
-            public void onChanged(List<Model_Posts> model_posts) {
+        if (binding == null) {
+            binding = FragmentPostBinding.inflate(inflater, container, false);
+            ViewModelPost viewModelPost = new ViewModelProvider(this).get(ViewModelPost.class);
+            viewModelPost.posts();
+            viewModelPost.mutableLiveData.observe(requireActivity(), new Observer<List<Model_Posts>>() {
+                @Override
+                public void onChanged(List<Model_Posts> model_posts) {
 
-                AdapterPosts adapter=new AdapterPosts(model_posts, new AdapterPosts.OnClickItem() {
-                    @Override
-                    public void onClick(String id) {
+                    AdapterPosts adapter = new AdapterPosts(model_posts, new AdapterPosts.OnClickItem() {
+                        @Override
+                        public void onClick(String id) {
 
-                        Log.e("TAG",id+"");
+                            Log.e("TAG", id + "");
 
-                        Bundle bundle=new Bundle();
-                        bundle.putString("idnum",id);
+                            Bundle bundle = new Bundle();
+                            bundle.putString("idnum", id);
 
-                        Navigation.findNavController(requireActivity(),R.id.fragment)
-                                .navigate(R.id.action_fragmentPost_to_fragmentDetaileCard,bundle);
+                            Navigation.findNavController(requireActivity(), R.id.fragment)
+                                    .navigate(R.id.action_fragmentPost_to_fragmentDetaileCard, bundle);
 
-                    }
-                });
+                        }
+                    });
 
-                binding.recyclrid.setLayoutManager(new LinearLayoutManager(requireContext()));
-                binding.recyclrid.setAdapter(adapter);
+                    binding.recyclrid.setLayoutManager(new LinearLayoutManager(requireContext()));
+                    binding.recyclrid.setAdapter(adapter);
 
 //                Log.e("TAG",model_posts.toString());
 
-            }
-        });
+                }
+            });
         }
-
-
 
 
         return binding.getRoot();
